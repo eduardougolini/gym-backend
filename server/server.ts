22cc6d0d20dd8fda9a9805c031f3e51d8e1c543b
Eduardo Ugolini <edu.ugolini2@gmail.com>
2018-11-15 15:20:13 -0200

    Algumas gambiarras para aceitar cors

--- server/server.ts
+++ server/server.ts
@@ -24,6 +24,25 @@ export class Server {
                 
                 this.application.use(restify.plugins.queryParser());
                 this.application.use(restify.plugins.bodyParser());
+                function unknownMethodHandler(req, res) {
+                    if (req.method.toLowerCase() === 'options') {
+                        console.log('received an options method request');
+                      var allowHeaders = ['Accept', 'Accept-Version', 'Content-Type', 'Api-Version', 'Origin', 'X-Requested-With']; // added Origin & X-Requested-With
+                  
+                      if (res.methods.indexOf('OPTIONS') === -1) res.methods.push('OPTIONS');
+                  
+                      res.header('Access-Control-Allow-Credentials', true);
+                      res.header('Access-Control-Allow-Headers', allowHeaders.join(', '));
+                      res.header('Access-Control-Allow-Methods', res.methods.join(', '));
+                      res.header('Access-Control-Allow-Origin', req.headers.origin);
+                  
+                      return res.send(204);
+                    }
+                    else
+                      return res.send(405);
+                  }
+                  
+                  this.application.on('MethodNotAllowed', unknownMethodHandler);
                 
                 //routes
                 for (let router of routers) {
